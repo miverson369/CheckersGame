0462afec8a4bb3e9fb284596f14287c775480afb
Mila Iverson <miverson369@outlook.com>
2026-06-19 17:28:21 -0700

    created basic checkers game with pieces, a console text base board, and move logic. the program takes user input.

---
+++ Tests/TestEm.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace CheckersGame.Tests
+{
+    public class [TestClass]
+    {
+        [TestMethod]
+        public void MovePiece_ValidMove_UpdatesBoard()
+        {
+            var game = new Game();
+
+            bool result = game.Move(5, 0, 4, 1);
+
+            Assert.IsTrue(result);
+            Assert.IsNull(game.Board.Grid[5, 0]);
+            Assert.IsNotNull(game.Board.Grid[4, 1]);
+        }
+    }
+}
